d8c5fa72840699b9cbed56f63c463b7fee5f7f5d
Yoshihiro Hayashi <yhayashi@ism.ac.jp>
2023-05-17 19:43:57 +0900

    modified setup.py

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Scientific/Engineering :: Chemistry',
         'Topic :: Scientific/Engineering :: Physics',
         'Topic :: Software Development :: Libraries :: Python Modules',
